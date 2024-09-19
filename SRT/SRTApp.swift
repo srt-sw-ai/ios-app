80bc68944de786acfe8679c2c03031b2c7b73a40
hoit1011 <parksm071011@gmail.com>
2024-09-19 23:57:52 +0900

    Initial Commit

---
+++ SRT/SRTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRTApp.swift
+//  SRT
+//
+//  Created by 박성민 on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SRTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SRTTests/SRTTests.swift
@@ -0,0 +1,17 @@
+//
+//  SRTTests.swift
+//  SRTTests
+//
+//  Created by 박성민 on 9/19/24.
+//
+
+import Testing
+@testable import SRT
+
+struct SRTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
